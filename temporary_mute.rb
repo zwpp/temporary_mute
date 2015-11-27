53d26e576100b5d9096c24d38cc97ae2617b56f1
zwpp <chromelevan@yahoo.co.jp>
2015-11-27 22:37:30 +0900

    Initial Commit

---
+++ temporary_mute.rb
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+Plugin.create(:temporary_mute) do
+  @muted_users = Set.new        # user_id
+
+  command(:temporary_mute_user_add,
+          name: "このユーザを一定時間をミュートする",
+          condition: -> _ { true },
+          visible: true,
+          role: :timeline ) do |m|
+              m.messages.each do |msg|
+                  temporary_mute(msg.user,UserConfig[:temporary_mute_seconds])
+              end
+          end
+
+  filter_show_filter do |messages|
+    [messages.select{ |m| !@muted_users.include?(m.user.id) }]
+  end
+
+  on_temporary_mute_muted do |user,time|
+    Plugin.call(:update, nil, [Message.new(:message => "@#{user.idname} を #{Time.now + time} までミュートします",
+                                           :system => true)])
+  end
+
+  on_temporary_mute_unmuted do |user|
+    Plugin.call(:update, nil, [Message.new(:message => "@#{user.idname}のミュートを解除します",
+                                           :system => true)])
+  end
+
+  class << self
+    def temporary_mute(user, mute_limit)
+      Plugin.call(:temporary_mute_muted, user, mute_limit)
+      @muted_users << user.id
+      Reserver.new(mute_limit){
+        temporary_unmute(user) }
+    end
+
+    def temporary_unmute(user)
+      Plugin.call(:temporary_mute_unmuted, user)
+      @muted_users.delete(user.id) 
+    end
+
+  # 一時ミュートする時間(秒)
+  UserConfig[:temporary_mute_seconds] ||= 1800 # 30分
+  end
+
+end
+
